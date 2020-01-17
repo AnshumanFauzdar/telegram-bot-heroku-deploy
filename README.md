7ac8a3e9a933f2265966fb5cb969b816a3463e73
Anshuman Fauzdar <fauzdaranshuman@gmail.com>
2020-01-17 19:08:40 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,65 @@
+# Intial Deploy to heroku
+Intially I was not able to push a simple bot to heroku, but yeah googling a lot made it work!
+
+### Step 1:
+- Create your bot like we have bot.py and write any python code that is intially working on your local machine!
+### Step 2:
+- Make a folder like *telegram-bot* and put *bot.py* in the folder
+### Step 3:
+- Make a blank python file named 
+   ```python
+   __init__.py
+### Step 4:
+- Make a *Procfile* this should be without any extension like .txt, you can go to view -> tick file name extensions and remove any extension
+   ```python
+   worker: python bot.py
+- Write this in Procfile by using notepad or any editor of your choice! Here bot.py is your python code!
+### Step 5:
+- Now we have to make a *requirements.txt* through which heroku will install the dependencies to make our bot work!
+- What to add in requirements.txt
+  - Mine looks like this:
+  ```python
+  future>=0.16.0
+  certifi
+  tornado>=5.1
+  cryptography
+  python-telegram-bot
+ 
+ Add anything which you have included in the python code!
+
+### Step 6:
+- Change directory to where you have made these files
+- now in git bash CLI, intialize a git
+  ```python
+  git init
+  
+### Step 7:
+- Now install heroku CLI
+- Next
+  ```python
+  heroku login
+  heroku create app_name
+- If you have already created app then select it:
+  ```python
+  heroku git:remote -a ap_pname
+- Or else continue:
+  ```python
+  git add -f bot.py Procfile requirements.txt __init__.py
+- ```python
+  git commit -m "Added Files"
+- Push files to heroku:
+  ```python
+  git push heroku master
+- If it is not working then try this one:
+   ```python
+   git push heroku master --force
+### At this point your bot should be running, you can check by
+-  ```python
+    heroku ps
+If it is not running then we have to reset dynos:
+- ```python
+  heroku ps:scale worker=0
+- ```python
+  heroku ps:scale worker=1
+Now it should be running fine! Enjoy :)  
+  
---
+++ bot.py
@@ -0,0 +1,68 @@
+import logging
+
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+
+# Define a few command handlers. These usually take the two arguments update and
+# context. Error handlers also receive the raised TelegramError object in error.
+def start(update, context):
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('hn bhai ke dard se?')
+
+
+def help(update, context):
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Nhi meri taraf mat dekhiye mai kuch nhi kar paaonga')
+
+def piracy(update, context):
+    update.message.reply_text('Abhi mai to kuch nhi kr skta, par aap @LeeluPradhanBot pe jaao!')
+
+
+def echo(update, context):
+    """Echo the user message."""
+    update.message.reply_text(update.message.text)
+
+
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+
+def main():
+    """Start the bot."""
+    # Create the Updater and pass it your bot's token.
+    # Make sure to set use_context=True to use the new context based callbacks
+    # Post version 12 this will no longer be necessary
+    updater = Updater("//TOKEN//", use_context=True)
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("piracy", piracy))
+
+    # on noncommand i.e message - echo the message on Telegram
+    dp.add_handler(MessageHandler(Filters.text, echo))
+
+    # log all errors
+    dp.add_error_handler(error)
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
