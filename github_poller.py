3f6b5be14798ad16e60b3c120f5968eb64635833
Boris Khodok <b_khodok@wargaming.net>
2023-11-09 17:23:40 +0100

    Added notifications on host is up Refactoring Now sends messages once in 5 minutes if host is still down

--- github_poller.py
+++ github_poller.py
@@ -1,4 +1,5 @@
 import asyncio
+from datetime import datetime
 from multiprocessing import get_logger
 import logging
 from typing import Any
@@ -34,25 +35,51 @@ class GitHubIssuePoller:
         self.g = Github(auth=self.auth)
         self.repo = self.g.get_repo('boriskhodok/wowsuptime')
         self.application = application
+        self.current_issues = {}
 
     async def poll_issues(self):
         while self.running:
             logger.info("Polling issues")
             try:
                 self.issues = self.repo.get_issues(state='open', labels=["status"])
+                if self.issues.totalCount < len(self.current_issues):
+                    await self._send_message_host_is_up()
                 if len(self.chats) > 0:
                     for chat_id in self.chats:
-                        for issue in self.issues:
-                            message = issue.title + "\n\n"
-                            message += issue.body
-                            await self.application.bot.send_message(chat_id, message,
-                                                                    parse_mode="Markdown",
-                                                                    disable_web_page_preview=True)
-                            await asyncio.sleep(1)
+                        await self._process_issues(chat_id)
                 logger.info("Got issues: " + str(self.issues.totalCount))
             except Exception as e:
                 logger.error("Exception: " + str(type(e)) + " " + e.message)
             await asyncio.sleep(30)
+
+    async def _process_issues(self, chat_id):
+        for issue in self.issues:
+            if issue.id not in self.current_issues.keys():
+                self.current_issues[issue.id] = {}
+                self.current_issues[issue.id]["time"] = datetime.now()
+                self.current_issues[issue.id]["issue"] = issue
+                message = self._issue_to_message(issue)
+
+                await self._send_message_to_chat(chat_id, message)
+
+            else:
+                if diff_minutes_round(datetime.now(), self.current_issues[issue.id]["time"]) > 5:
+                    message = self._issue_to_message(issue)
+                    message = message.replace("is", "is still")
+
+                    await self._send_message_to_chat(chat_id, message)
+
+
+    @staticmethod
+    def _issue_to_message(issue) -> str:
+        message = issue.title
+        return message
+
+    async def _send_message_to_chat(self, chat_id, message):
+        await self.application.bot.send_message(chat_id, message,
+                                                parse_mode="Markdown",
+                                                disable_web_page_preview=True)
+        await asyncio.sleep(1)
 
     @classmethod
     def add_chat(cls, chat_id: int):
@@ -67,3 +94,29 @@ class GitHubIssuePoller:
 
         self.running = False
         logger.debug("Shut down of Poller complete")
+
+    async def _send_message_host_is_up(self):
+        issue_ids_from_api = []
+        for issue in self.issues:
+            issue_ids_from_api.append(issue.id)
+
+        issue_ids_to_remove = []
+        for issue_id in self.current_issues.keys():
+            if issue_id not in issue_ids_from_api:
+                message = self._issue_to_message(self.current_issues[issue_id]["issue"])
+                message = message.replace("🛑", "🟢")
+                message = message.replace("down", "up")
+
+                for chat_id in self.chats:
+                    await self._send_message_to_chat(chat_id, message)
+                    await asyncio.sleep(1)
+
+                issue_ids_to_remove.append(issue_id)
+
+        for issue_id in issue_ids_to_remove:
+            self.current_issues.pop(issue_id)
+
+
+def diff_minutes_round(date1: datetime, date2: datetime) -> int:
+    diff = (date1 - date2).seconds / 60
+    return abs(round(diff))
